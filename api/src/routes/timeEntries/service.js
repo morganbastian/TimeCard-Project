02f22ef73529e1be52c779e74770fc132872cc2f
Morgan Bastian <morganbastian07@gmail.com>
2024-01-03 15:18:54 -0500

    total hours accumulate properly

--- api/src/routes/timeEntries/service.js
+++ api/src/routes/timeEntries/service.js
@@ -32,88 +32,99 @@ exports.findTimeEntryById = async (id) => {
 // }
 
 exports.addNewTimeEntry = async (newEntry) => {
-  const date = new Date(newEntry.date);
-  const formattedDate = date.toISOString().slice(0, 10);
-  //get time in hh:mm:ss format
-  const startTime = new Date(newEntry.startTime).toISOString().slice(11, 19);
-  const endTime = new Date(newEntry.endTime).toISOString().slice(11, 19);
-  // split time into hours minutes second with destructured array
-  const [startHours, startMinutes, startSeconds] = startTime.split(':');
-  const [endHours, endMinutes, endSeconds] = endTime.split(':');
-  // convert to 24 if PM and not 12
-  const formattedStartHour = startHours;
-  const formattedEndHour = endHours;
-  // turn back into a time string
-  const formattedStartTime = `${formattedStartHour}:${startMinutes}:${startSeconds}`;
-  const formattedEndTime = `${formattedEndHour}:${endMinutes}:${endSeconds}`;
+	const date = new Date(newEntry.date)
+	const formattedDate = date.toISOString().slice(0, 10)
+	//get time in hh:mm:ss format
+	const startTime = new Date(newEntry.startTime).toISOString().slice(11, 19)
+	const endTime = new Date(newEntry.endTime).toISOString().slice(11, 19)
+	// split time into hours minutes second with destructured array
+	const [startHours, startMinutes, startSeconds] = startTime.split(':')
+	const [endHours, endMinutes, endSeconds] = endTime.split(':')
+	// convert to 24 if PM and not 12
+	const formattedStartHour = startHours
+	const formattedEndHour = endHours
+	// turn back into a time string
+	const formattedStartTime = `${formattedStartHour}:${startMinutes}:${startSeconds}`
+	const formattedEndTime = `${formattedEndHour}:${endMinutes}:${endSeconds}`
 
-  // Convert to Date objects
-  const startDate = new Date(`1970-01-01T${formattedStartTime}Z`);
-  const endDate = new Date(`1970-01-01T${formattedEndTime}Z`);
+	// Convert to Date objects
+	const startDate = new Date(`1970-01-01T${formattedStartTime}Z`)
+	const endDate = new Date(`1970-01-01T${formattedEndTime}Z`)
 
-  //get the difference for start and end time
-  const totalHours = (endDate - startDate) / (1000 * 60 * 60);
+	//get the difference for start and end time
+	const totalHours = (endDate - startDate) / (1000 * 60 * 60)
 
-  newEntry.date = formattedDate;
-  newEntry.startTime = formattedStartTime;
-  newEntry.endTime = formattedEndTime;
-  newEntry.totalHours = totalHours;
-  newEntry.employeeId = 1;
+	newEntry.date = formattedDate
+	newEntry.startTime = formattedStartTime
+	newEntry.endTime = formattedEndTime
+	newEntry.totalHours = totalHours
+	newEntry.employeeId = 1
 
-  const createdTimeEntry = await knex('time_entries').insert(newEntry);
-  return createdTimeEntry;
+	const createdTimeEntry = await knex('time_entries').insert(newEntry)
+	return createdTimeEntry
 }
 
-//selects all time entries 
+//selects all time entries
 exports.findAllTimeEntries = async () => {
-	const entries = await knex('time_entries').select('*');
+	const entries = await knex('time_entries').select('*')
 
 	const formattedData = entries.map((item) => {
-			const date = new Date(item.date);
-			const year = date.getFullYear();
-			const month = String(date.getMonth() + 1).padStart(2, '0');
-			const day = String(date.getDate()).padStart(2, '0');
-			const formattedDate = `${year}-${month}-${day}`; // YYYY-MM-DD format
+		const date = new Date(item.date)
+		const year = date.getFullYear()
+		const month = String(date.getMonth() + 1).padStart(2, '0')
+		const day = String(date.getDate()).padStart(2, '0')
+		const formattedDate = `${year}-${month}-${day}` // YYYY-MM-DD format
 
-  // Check if startTime and endTime are not null
-  if (item.startTime && item.endTime) {
-    const formattedStartTime = convertTo24HourFormat(item.startTime, formattedDate);
-    const formattedEndTime = convertTo24HourFormat(item.endTime, formattedDate);
+		// Check if startTime and endTime are not null
+		if (item.startTime && item.endTime) {
+			const formattedStartTime = convertTo24HourFormat(
+				item.startTime,
+				formattedDate
+			)
+			const formattedEndTime = convertTo24HourFormat(
+				item.endTime,
+				formattedDate
+			)
 
-    // Calculate time difference
-    const startTimestamp = new Date(formattedStartTime).getTime();
-    const endTimestamp = new Date(formattedEndTime).getTime();
-    const hoursDiff = (endTimestamp - startTimestamp) / (1000 * 60 * 60); // Convert milliseconds to hours
+			// Calculate time difference
+			const startTimestamp = new Date(formattedStartTime).getTime()
+			const endTimestamp = new Date(formattedEndTime).getTime()
+			const hoursDiff = (endTimestamp - startTimestamp) / (1000 * 60 * 60) // Convert milliseconds to hours
 
-    return { ...item, date: formattedDate, startTime: formattedStartTime, endTime: formattedEndTime, totalHours: hoursDiff };
-  } else {
-    throw new Error('startTime or endTime is null');
-  }
-});
+			return {
+				...item,
+				date: formattedDate,
+				startTime: formattedStartTime,
+				endTime: formattedEndTime,
+				totalHours: hoursDiff,
+			}
+		} else {
+			throw new Error('startTime or endTime is null')
+		}
+	})
 
-	return formattedData;
-};
-
+	return formattedData
+}
 
 function convertTo24HourFormat(time, date) {
-  let hours, minutes, ampm;
-  const match12Hour = time.match(/(\d+):(\d+)(\sAM|\sPM)/);
-  const match24Hour = time.match(/(\d+):(\d+):(\d+)/);
+	let hours, minutes, ampm
+	const match12Hour = time.match(/(\d+):(\d+)(\sAM|\sPM)/)
+	const match24Hour = time.match(/(\d+):(\d+):(\d+)/)
 
-  if (match12Hour) {
-    [hours, minutes, ampm] = match12Hour.slice(1);
-    if (ampm.trim() === 'PM' && hours !== '12') {
-      hours = (parseInt(hours, 10) + 12).toString();
-    } else if (ampm.trim() === 'AM' && hours === '12') {
-      hours = '00';
-    }
-  } else if (match24Hour) {
-    [hours, minutes] = match24Hour.slice(1);
-  } else {
-    throw new Error(`Invalid time format: ${time}`);
-  }
+	if (match12Hour) {
+		;[hours, minutes, ampm] = match12Hour.slice(1)
+		if (ampm.trim() === 'PM' && hours !== '12') {
+			hours = (parseInt(hours, 10) + 12).toString()
+		} else if (ampm.trim() === 'AM' && hours === '12') {
+			hours = '00'
+		}
+	} else if (match24Hour) {
+		;[hours, minutes] = match24Hour.slice(1)
+	} else {
+		throw new Error(`Invalid time format: ${time}`)
+	}
 
-  return `${date}T${hours}:${minutes}:00`; // Return in YYYY-MM-DDTHH:MM:SS format
+	return `${date}T${hours}:${minutes}:00` // Return in YYYY-MM-DDTHH:MM:SS format
 }
 
 exports.modifyTimeEntry = async (timeEntryData, id) => {
@@ -121,5 +132,3 @@ exports.modifyTimeEntry = async (timeEntryData, id) => {
 	console.log(timeEntryData)
 	return await knex('time_entries').update(timeEntryData).where('id', id) // return the data you need
 }
-
-
--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -1,101 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers';
-import { TimePicker } from '@mui/x-date-pickers';
-import { Box, Button, Typography } from '@mui/material';
-import { createNewTimeEntry, getTimeEntryById, updateTimeEntry, getAllTimeEntries} from '../../utility/api';
+import React, { useState, useEffect } from 'react'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DateCalendar } from '@mui/x-date-pickers'
+import { TimePicker } from '@mui/x-date-pickers'
+import { Box, Button, Typography } from '@mui/material'
+import {
+	createNewTimeEntry,
+	getTimeEntryById,
+	updateTimeEntry,
+	getAllTimeEntries,
+} from '../../utility/api'
 
 function Home(props) {
-  // State Variables
-  const [date, setDate] = useState(null);
-  const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
-  const [totalHours, setTotalHours] = useState(0);
+	// State Variables
+	const [date, setDate] = useState(null)
+	const [startTime, setStartTime] = useState(null)
+	const [endTime, setEndTime] = useState(null)
+	const [totalHours, setTotalHours] = useState(0)
+  const [dailyHours, setDailyHours] = useState(0)
 
-useEffect(() => {
-  //get all time entries data
-  const getAllTimeEntriesData = async () => {
-    const response = await getAllTimeEntries();
-    console.log('response:' ,response);
-  };
-  getAllTimeEntriesData();
-}, []);
+	useEffect(() => {
+		//get all time entries data
+		const getAllTimeEntriesData = async () => {
+			const response = await getAllTimeEntries()
+			console.log('response:', response)
 
-  //create a new time entry on submit
-const handleSubmit = async () => {
-  
-  const startTimestamp = new Date(startTime).getTime();
-  const endTimestamp = new Date(endTime).getTime();
-  const hoursDiff = (endTimestamp - startTimestamp) / (1000 * 60 * 60); // Convert milliseconds to hours
+			// Calculate total hours
+			let total = 0
+			response.forEach((entry) => {
+				const startTimestamp = new Date(entry.startTime).getTime()
+				const endTimestamp = new Date(entry.endTime).getTime()
+				const hoursDiff = (endTimestamp - startTimestamp) / (1000 * 60 * 60) // Convert milliseconds to hours
+				total += hoursDiff
+			})
 
-    const timeEntryData = {
-      date: date,
-      startTime: startTime,
-      endTime: endTime,
-      totalHours: hoursDiff,
-    };
-    try {
-      const response = await createNewTimeEntry(timeEntryData);
-      console.log(response);
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-    //reset all fields on reset
-const handleReset = () => {
-    setDate(null);
-    setStartTime(null);
-    setEndTime(null);
-    setTotalHours(0);
-  }
+			// Set total hours
+			setTotalHours(total)
+		}
+		getAllTimeEntriesData()
+	}, [])
 
+	//create a new time entry on submit
+	const handleSubmit = async () => {
+		const startTimestamp = new Date(startTime).getTime()
+		const endTimestamp = new Date(endTime).getTime()
+		const hoursDiff = (endTimestamp - startTimestamp) / (1000 * 60 * 60) // Convert milliseconds to hours
 
-  return (
-    <div>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DateCalendar
-          value={date}
-          required
-          onChange={(date) => setDate(date)}
-        />
-        <Box 
-          display="flex" 
-          flexDirection="column" 
-          justifyContent="center" 
-          alignItems="center" 
-          height="20vh"
-        >
-          <TimePicker 
-            label="Start Time" 
-            value={startTime} 
-            onChange={(time) => setStartTime(time)} 
-          />
-          <TimePicker 
-            label="End Time" 
-            value={endTime}
-            onChange={(time) => setEndTime(time)}
-            sx={{margin: 2}}
-          />
-        </Box>
-      </LocalizationProvider>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </div>
-        <Typography sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '5vh'}} variant="h6">
-          Accumulated Total Hours: {totalHours.toFixed(2)}
-        </Typography>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-        <Button variant="contained" color="primary" onClick={handleReset}>
-          Reset
-        </Button>
-      </div>
-    </div>
-  )
+		const timeEntryData = {
+			date: date,
+			startTime: startTime,
+			endTime: endTime,
+			totalHours: hoursDiff,
+		}
+		try {
+			const response = await createNewTimeEntry(timeEntryData)
+			console.log(response)
+
+			// Add hours of new time entry to totalHours
+			setTotalHours((prevTotalHours) => prevTotalHours + hoursDiff)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	//reset all fields on reset
+	const handleReset = () => {
+		setDate(null)
+		setStartTime(null)
+		setEndTime(null)
+		setTotalHours(0)
+	}
+
+	return (
+		<div>
+			<LocalizationProvider dateAdapter={AdapterDateFns}>
+				<DateCalendar
+					value={date}
+					required
+					onChange={(date) => setDate(date)}
+				/>
+				<Box
+					display='flex'
+					flexDirection='column'
+					justifyContent='center'
+					alignItems='center'
+					height='20vh'
+				>
+					<TimePicker
+						label='Start Time'
+						value={startTime}
+						onChange={(time) => setStartTime(time)}
+					/>
+					<TimePicker
+						label='End Time'
+						value={endTime}
+						onChange={(time) => setEndTime(time)}
+						sx={{ margin: 2 }}
+					/>
+				</Box>
+			</LocalizationProvider>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '10vh',
+				}}
+			>
+				<Button variant='contained' color='primary' onClick={handleSubmit}>
+					Submit
+				</Button>
+			</div>
+			<Typography
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '5vh',
+				}}
+				variant='h6'
+			>
+				Accumulated Total Hours: {totalHours}
+			</Typography>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '10vh',
+				}}
+			>
+				<Button variant='contained' color='primary' onClick={handleReset}>
+					Reset
+				</Button>
+			</div>
+		</div>
+	)
 }
 
-export default Home;
-
+export default Home
